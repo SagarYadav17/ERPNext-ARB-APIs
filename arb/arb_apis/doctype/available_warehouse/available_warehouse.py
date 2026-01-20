526ddb8061549271a32786b00c3c0b6487518108
Yuvraj Singh Kathait <ys36218@gmail.com>
2026-01-20 14:25:44 +0530

    adding new doctype

---
+++ arb/arb_apis/doctype/available_warehouse/available_warehouse.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026,  Kerol Systems and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class AvailableWarehouse(Document):
+	pass
